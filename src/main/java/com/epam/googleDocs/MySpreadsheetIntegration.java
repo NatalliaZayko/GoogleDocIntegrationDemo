82a6882cd41dec95d3d76cc4865a351e5bdd6c49
Natallia_Zayko <natallia_zayko@epam.com>
2014-12-30 17:30:55 +0300

    refresh

--- src/main/java/com/epam/googleDocs/MySpreadsheetIntegration.java
+++ src/main/java/com/epam/googleDocs/MySpreadsheetIntegration.java
@@ -7,17 +7,24 @@ import com.google.gdata.util.*;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.security.GeneralSecurityException;
+import java.util.Scanner;
 
 public class MySpreadsheetIntegration {
 
 	public static void main(String[] args) throws AuthenticationException,
 			MalformedURLException, IOException, ServiceException,
 			GeneralSecurityException {
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Week:");
+		int week = in.nextInt();
+		System.out.println("Course:");
+		String course = in.next();	
+		in.close();			
 		Steps.authorize();
-		Steps.getListNames(1, "XML+XSD");
+		Steps.getListNames(week, course);
 		Steps.loginInClassMarket();
-
-
+		
+		
+		
 	}
 }
